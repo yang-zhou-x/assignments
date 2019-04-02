293057802ac16cc29f89ad569f6f1450a9ec14d5
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-04-02 23:04:00 +0800

    Update lstm.py

--- 4.lstm_sentiment_analysis/lstm.py
+++ 4.lstm_sentiment_analysis/lstm.py
@@ -5,21 +5,29 @@ Created on 2019
 """
 
 import os
+import time
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from jieba import lcut
 
 from keras.preprocessing import sequence, text
-from keras.models import Sequential, Model
-from keras.layers import Dense, Embedding, LSTM, Flatten, Input
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Dropout
+from keras.layers import Embedding, LSTM
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
 
 # 设置模型参数
 max_features = 10000  # 词汇表的单词总数
-max_len = 30  # 单个序列的长度
-batch_size = 32
-optimizer = 'adam'
-epoch = 2
+embedding_dim = 128  # 词向量的维度
+max_len = 30  # 序列的长度
+output_dim = 64  # LSTM的输出维度
+drop_out_rate = 0.2
+batch_size = 64
+optimizer = Adam(lr=.002, epsilon=.001)
+epoch = 10
+metric = ['accuracy']
 
 '''
 1.数据准备
@@ -37,16 +45,32 @@ data['len'] = data['review'].map(lambda x: len(x))
 print(data[['cat', 'len']].groupby(by='cat').agg([np.median, np.mean, np.std]))
 
 
+# 修饰器，计时
+def time_func(desc=''):
+    def timeit(func):
+        def aux(*args, **kwargs):
+            t0 = time.time()
+            res = func(*args, **kwargs)
+            t1 = time.time()
+            print(desc + f'耗时{round(t1 - t0, 3)}s.')
+            return res
+        return aux
+    return timeit
+
+
 # 减少数据集的内存占用
+@time_func('减少数据集内存占用')
 def reduce_memory_usage(df):
     """
     通过合理降低数字精确度，减少数据集对于内存的占用。
     不考虑时间型数据。
-    :param df: pandas.DataFrame
-    :return: pandas.DataFrame
+    # 参数
+        df: pandas.DataFrame
+    # return
+        pandas.DataFrame
     """
     start_mem = df.memory_usage().sum() / 1024
-    print(f'数据集占用内存大小为： {start_mem} KB.')
+    print(f'数据集初始占用内存大小为： {round(start_mem, 2)} KB.')
     for col in df.columns:
         col_type = df[col].dtype  # 获取该列的数据类型
         if col_type != object:
@@ -72,7 +96,7 @@ def reduce_memory_usage(df):
             df[col] = df[col].astype('str')
             # df[col] = df[col].astype('category')
     end_mem = df.memory_usage().sum() / 1024
-    print(f'数据集占用内存大小为： {end_mem} KB.')
+    print(f'数据集处理后占用内存大小： {round(end_mem, 2)} KB.')
     print(f'减少了{round((start_mem - end_mem) / start_mem, 4) * 100}%.')
     return df
 
@@ -81,10 +105,12 @@ data = reduce_memory_usage(data)
 
 
 # 获取停用词、标点符号
+@time_func('获取停用词/标点符号')
 def get_stop_words():
     """
     读取停用词表，含标点符号
-    :return: 列表
+    # return
+        list[str], 停用词表/符号
     """
 
     def iter_file(path):
@@ -106,13 +132,16 @@ stops = get_stop_words()
 def pre_transform(string, stop_words):
     """
     对一条评论进行分词、去除停用词和标点符号后，以空格分隔重新拼接
-    :return: 字符串
+    # return
+        str, 分好词的评论
     """
     return ' '.join(x for x in lcut(string) if x not in stop_words)
 
 
 print('进行分词...')
+t = time.time()
 data.loc[:, 'review'] = data['review'].map(lambda x: pre_transform(x, stops))
+print(f'分词耗时{round(time.time() - t, 3)}s.')
 
 # 将文本转化为整数序列
 print('将文本转化为整数序列...')
@@ -137,35 +166,40 @@ print(f'x_test shape: {x_test.shape}')
 '''
 print('建立模型...')
 model = Sequential()
-model.add(Embedding(input_dim=max_features, output_dim=128, input_length=max_len))
-model.add(LSTM(units=64, activation='tanh', return_sequences=True, dropout=0.2, recurrent_dropout=0.2))
+model.add(Embedding(input_dim=max_features, output_dim=embedding_dim, input_length=max_len))
+# 使用全部序列(return_sequences=True)
+model.add(LSTM(units=output_dim, recurrent_activation='hard_sigmoid', return_sequences=True,
+               recurrent_dropout=drop_out_rate))
+model.add(Activation('tanh'))
+model.add(Dropout(drop_out_rate))
 model.add(Flatten())
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
 
 print('模型概况：')
 model.summary()
 
+# 模型编译
 model.compile(optimizer=optimizer,
               loss='binary_crossentropy',
-              metrics=['accuracy'])
+              metrics=metric)
+
 
 '''
 3.训练模型并评价
 '''
 print('训练模型并评价...')
+callback = [EarlyStopping(monitor='val_loss', patience=3, verbose=1)]
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epoch,
           verbose=1,
+          callbacks=callback,
           validation_data=(x_test, y_test))
 
-# score, acc = model.evaluate(x_test, y_test, batch_size=batch_size)
-# print(f'测试集误差：{score}')
-# print(f'测试集准确率：{acc}')
-model.predict(x_test[0].reshape(-1, max_len))
 
 '''
-4.模型持久化 model persistence
+4.保存模型 model persistence
 '''
 save_dir = os.path.join(init_path, 'saved_models')
 if not os.path.isdir(save_dir):
