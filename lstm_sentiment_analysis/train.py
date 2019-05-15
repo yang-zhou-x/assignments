8e2c635136ee4b70649821cf489303dd1c9ea4aa
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-05-15 22:03:26 +0800

    Update train.py

--- lstm_sentiment_analysis/train.py
+++ lstm_sentiment_analysis/train.py
@@ -1,1 +1,16 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    :   2019
+@Author  :   ZHOU, YANG
+@Contact :   yzhou0000@gmail.com
+'''
 
+import os
+import pickle
+
+from keras.models import load_model
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+import preprocess as pp
+import models as m
