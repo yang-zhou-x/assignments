6b5e10cdabaf4b0978baa0f2471c0fe3a6a0f01e
Yang Zhou <45114669+yang-zhou-x@users.noreply.github.com>
2019-07-28 11:41:45 +0800

    Update train.py

--- lstm_sentiment_analysis/train.py
+++ lstm_sentiment_analysis/train.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 '''
+This module is used for training models.
+
 @Time    :   2019
 @Author  :   ZHOU, YANG
 @Contact :   yzhou0000@gmail.com
@@ -10,13 +12,11 @@ import pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from keras.models import load_model
-from keras.optimizers import Adam
+import keras.optimizers as optim
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 import matplotlib.pyplot as plt
-
 import preprocess as pp
 import models as m
-
 
 # Parameters
 seed = 2019
@@ -26,34 +26,46 @@ embedding_dim = 128  # 词向量的维度
 sequence_len = 30  # 序列的长度
 lstm_units = 64  # LSTM的输出维度
 dropout_rate = 0.2
+fc_units = -1
 batch_size = 64
-optimizer = Adam(lr=.002, epsilon=.001)
+optimizer = optim.Adam(lr=.002, epsilon=.001)
 epochs = 50  # 实际epoch<=50
 metric = ['accuracy']
-model_name = 'lstm_shpping.hdf5'
-tokenizer_name = 'tokenizer.pickle'
+
+train_test_name = 'online_shopping_10_cats.csv'
+stopwords_name = 'cn_stopwords_punctuations.csv'
+model_name = f'lstm-{sequence_len}.hdf5'
+tokenizer_name = f'tokenizer-{max_features}.pickle'
+
 
 # Paths
 init_path = os.getcwd()
+
 data_dir = os.path.join(init_path, 'datasets')
-train_test_path = os.path.join(data_dir, 'online_shopping_10_cats.csv')
-stopwords_path = os.path.join(data_dir, 'cn_stopwords_punctuations.csv')
+train_test_path = os.path.join(data_dir, train_test_name)
+stopwords_path = os.path.join(data_dir, stopwords_name)
+
 save_dir = os.path.join(init_path, 'saved_models')
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
 model_path = os.path.join(save_dir, model_name)
 tokenizer_path = os.path.join(save_dir, tokenizer_name)
 
+
 # Model
 last_layer_options = m.get_last_layer_options(num_classes)
-model = m.lstm_model(num_features=max_features,
+model = m.lstm_model(last_layer_options=last_layer_options,
+                     num_features=max_features,
                      embedding_dim=embedding_dim,
                      sequence_len=sequence_len,
                      lstm_units=lstm_units,
                      dropout_rate=dropout_rate,
-                     last_layer_options=last_layer_options)
+                     fc_units=fc_units)
 model.summary()
 model.compile(optimizer=optimizer,
               loss=last_layer_options[2],
               metrics=metric)
+
 
 # Data
 df_data = pd.read_csv(train_test_path, header=0)
@@ -67,15 +79,21 @@ print(df_data[['cat', 'len']]
 
 df_data = pp.reduce_memory_usage(df_data)
 stopwords = pp.get_stopwords(stopwords_path)
-x = pp.tokenize_texts(df_data['review'].values, stopwords)
-y, _ = pp.encode_y(df_data['label'].values, num_classes)
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,
+
+x_texts = df_data['review'].values
+y_labels = df_data['label'].values
+
+x_texts = pp.tokenize_texts(x_texts, stopwords)
+y_labels, _ = pp.encode_y(y_labels, num_classes)
+x_train, x_test, y_train, y_test = train_test_split(x_texts, y_labels,
+                                                    test_size=0.2,
                                                     random_state=seed)
 
-x_train, x_test, tokenizer = pp.vectorize_texts_to_sequences(
+x_train, x_test, tokenizer = pp.texts_to_sequence_vectors(
     x_train, sequence_len, dict_size=max_features, x_test=x_test)
 with open(tokenizer_path, 'wb') as f:
     pickle.dump(tokenizer, f)
+
 
 # Train
 callback = [EarlyStopping('val_acc', patience=5),
